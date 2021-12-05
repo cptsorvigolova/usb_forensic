59d8bd140e3b080b06a963570c4bf0839352f110
klimburov <klimburov@mail.ru>
2021-12-05 22:24:13 +0400

    Update CollectArtifacts.ps1

--- CollectArtifacts.ps1
+++ CollectArtifacts.ps1
@@ -1,11 +1,11 @@
 Write-Host "Rights check..."
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-[Security.Principal.WindowsBuiltInRole] "Administrator")) {
-Write-Warning "Administrator rights required. Open PowerShell with administrator rights and run again."
-Break
+      [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Write-Warning "Administrator rights required. Open PowerShell with administrator rights and run again."
+  Break
 }
 else {
-Write-Host "Continue..." -ForegroundColor Green
+  Write-Host "Continue..." -ForegroundColor Green
 }
 
 $currentDriveLetter = Get-Location | Split-Path -Qualifier;
@@ -19,8 +19,23 @@ $location = Get-Location;
 
 New-Item -ItemType Directory -Force -Path $destination;
 
-Get-ItemProperty -Path HKLM:\System\CurrentControlSet\Enum\USBSTOR\*\* | 
-select FriendlyName, ClassGUID, Service, PSChildName | 
+Get-ChildItem -Path HKLM:\System\CurrentControlSet\Enum\USBSTOR\*\* |
+Select-Object @{
+  label      = 'PSChildName'
+  expression = { $_ | Get-ItemProperty | Select-Object PSChildName }
+}, 
+@{
+  label      = 'FriendlyName'
+  expression = { $_.GetValue("FriendlyName") }
+},
+@{
+  label      = 'ClassGUID'
+  expression = { $_.GetValue("ClassGUID") }
+},
+@{
+  label      = 'DiskId'
+  expression = { $_.OpenSubKey("Device Parameters").OpenSubKey("Partmgr").GetValue("DiskId").ToString() }
+} | 
 Export-Clixml -Path "$destination\Devices.xml";
 
 Set-Location "$kapeLocation\Modules\bin";
@@ -38,10 +53,10 @@ Set-Location "$kapeLocation\Modules\bin";
 Set-Location $kapeLocation;
 
 $disks = Get-Partition | 
-Where-Object {$_.DiskPath -like "\\?\scsi*" -and $_.DriveLetter -ne 0} | 
-select DriveLetter
-foreach ($disk in $disks){
-    .\kape.exe --tsource $($disk.DriveLetter) --target FileSystem --tdest "$destination\USNJournal\$($disk.DriveLetter)";
-    .\kape.exe --msource "$destination\USNJournal\$($disk.DriveLetter)" --module MFTECmd --mdest "$destination\USNJournal\$($disk.DriveLetter)\Parsed";
-    }
+Where-Object { $_.DiskPath -like "\\?\scsi*" -and $_.DriveLetter -ne 0 } | 
+Select-Object DriveLetter
+foreach ($disk in $disks) {
+  .\kape.exe --tsource $($disk.DriveLetter) --target FileSystem --tdest "$destination\USNJournal\$($disk.DriveLetter)";
+  .\kape.exe --msource "$destination\USNJournal\$($disk.DriveLetter)" --module MFTECmd --mdest "$destination\USNJournal\$($disk.DriveLetter)\Parsed";
+}
 Set-Location $location;
